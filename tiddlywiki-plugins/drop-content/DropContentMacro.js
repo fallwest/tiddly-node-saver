f2937dbbcd22d24f70663cd3726d5e4d9ce373f0
James Westfall <james.westfall@neat.no>
2025-04-12 12:09:29 +0200

    Greatly simplify macro by using HTML5DownloadSaveFile and eliminating mime type

--- tiddlywiki-plugins/drop-content/DropContentMacro.js
+++ tiddlywiki-plugins/drop-content/DropContentMacro.js
@@ -1,11 +1,11 @@
 /***
 |Description|Insert text documents as new tiddlers that download content when opened|
-|Source     |https://github.com/fallwest/tiddly-node-saver/blob/james.westfall/drop_content_plugin/tiddlywiki-plugins/drop-content/DropContentMacro.js|
+|Source     |https://github.com/fallwest/tiddlynodesaver/blob/james.westfall/drop_content_plugin/tiddlywikiplugins/dropcontent/DropContentMacro.js|
 |Version    |0.0.1|
 |Author     |James Westfall|
-|License    |[[MIT|https://github.com/fallwest/tiddly-node-saver/blob/james.westfall/drop_content_plugin/LICENSE]]|
+|License    |[[MIT|https://github.com/fallwest/tiddlynodesaver/blob/james.westfall/drop_content_plugin/LICENSE]]|
 !!!Usage
-Insert the {{{<<DropContent>>}}} macro in a tiddler and drag text files onto the drop zone. Chose the appropriate mime-type before drag-and-dropping:
+Insert the {{{<<DropContent>>}}} macro in a tiddler and drag text files onto the drop zone:
 
 <<DropContent>>
 ***/
@@ -13,17 +13,16 @@ Insert the {{{<<DropContent>>}}} macro in a tiddler and drag text files onto the
     config.macros.DropContent = {
 
         handler: function (place, macroName, params, wikifier, paramString, tiddler) {
-            const holder = createTiddlyElement(place, "div", "holder", null, "Drop your file here", {
+            var holder = createTiddlyElement(place, "div", "holder", null, "Drop your file here", {
                 style: "border-style: dotted;width:150px;padding: 50px 0;text-align: center;"
             });
     
-            const mimetype = createTiddlyElement(place, "select", "mimetype");
-    
-            createTiddlyElement(mimetype, "option", null, null, "text/calendar");
-            createTiddlyElement(mimetype, "option", null, null, "text/plain");
-            createTiddlyElement(mimetype, "option", null, null, "application/json");
-            createTiddlyElement(mimetype, "option", null, null, "text/csv");
-            createTiddlyElement(mimetype, "option", null, null, "text/xml");
+            holder.ondragover = function () {
+                return false;
+            };
+            holder.ondragend = function () {
+                return false;
+            };
     
             holder.ondrop = function (e) {
                 this.className = "";
@@ -32,9 +31,8 @@ Insert the {{{<<DropContent>>}}} macro in a tiddler and drag text files onto the
                 const file = e.dataTransfer.files[0];
                 const reader = new FileReader();
                 reader.onload = function (event) {
-                    this.content = "<script>\ndownload_content(\"" + file.name + "\", ";
-                    this.content += "\`" + event.target.result + "\`, ";
-                    this.content += "\"" + mimetype.value + "\")\n</script>";
+                    this.content = "<script>\nHTML5DownloadSaveFile(\"" + file.name + "\", ";
+                    this.content += "\`" + event.target.result + "\`)\n</script>";
                     const tiddlername = store.tiddlerExists(file.name) ? file.name + "_" + Date.now() : file.name;
                     store.saveTiddler(tiddlername, tiddlername, this.content, config.options.txtUserName, new Date());
                 };
@@ -43,20 +41,4 @@ Insert the {{{<<DropContent>>}}} macro in a tiddler and drag text files onto the
             }
         }
     };
-    
-    function download_content(filename, text, type = "text/plain") {
-        const a = document.createElement("a");
-        a.style.display = "none";
-        document.body.appendChild(a);
-    
-        a.href = window.URL.createObjectURL(
-            new Blob([text], { type })
-        );
-    
-        a.setAttribute("download", filename);
-        a.click();
-    
-        window.URL.revokeObjectURL(a.href);
-        document.body.removeChild(a);
-    }
-    //}}}
+//}}}
